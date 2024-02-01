d3b7d2ba4987ce241d6c0b91df1f56a628345a31
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2024-02-01 09:23:49 +0100

    fix: safe area rendering artifacts

--- board.py
+++ board.py
@@ -146,11 +146,13 @@ class GameBoard:
         width, height = self.size
         tile_width = window_width / width
         tile_height = window_height / height
+        pygame.draw.rect(surface, "#efefef", (0, 0, tile_width * (self.safe_zone_width + 1), window_height))
+        pygame.draw.rect(surface, "#efefef", (window_width - tile_width * (self.safe_zone_width + 1), 0, tile_width * (self.safe_zone_width + 1), window_height))
         for x, column in enumerate(self.board):
             for y, tile in enumerate(column):
                 color = TILE_COLOR[tile]
-                if tile == TILE_EMPTY and (x <= self.safe_zone_width or x >= width - self.safe_zone_width - 1):
-                    color = "#efefef"
+                if tile == TILE_EMPTY:
+                    continue
 
                 rect = (x * tile_width, y * tile_height, tile_width, tile_height)
                 pygame.draw.rect(surface, color, rect)
